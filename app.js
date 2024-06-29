e4aabd64d379bc8cebe4af176dcc560a5976cc5f
Ademola Adeagbo <38380242+demmybite@users.noreply.github.com>
2024-06-29 22:59:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const usernameElement = document.getElementById("username");
 const emailElement = document.getElementById("email");
 const websiteLink = document.getElementById("websiteLink");
 
-// Replace with your actual name and email
+
 nameElement.textContent = "Name: Ademola Adeagbo";
 usernameElement.textContent = "Username: Demmybammy";
 emailElement.textContent = "Email: demmybite@gmail.com";
 
-// Replace with the URL of the website you want to link
 websiteLink.href = "https://hng.tech";
 websiteLink.textContent = "Visit Internship Website";
